4e1be03938ecee5c9e8860483140b78e3a8e998e
Gilles Fabio <gilles@gillesfabio.com>
2014-03-02 10:58:20 +0100

    Add gulp tasks and build folder.

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var path = require('path');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var clean = require('gulp-clean');
+var express = require('express');
+
+var SERVER_PORT = 3000;
+var BUILD = 'build';
+var SCRIPTS_CONCAT = 'scripts.js';
+var STYLES_CONCAT = 'styles.css';
+var MODERNIZR = 'vendor/foundation/js/vendor/modernizr.js';
+var SCRIPTS = [
+  'vendor/foundation/js/vendor/jquery.js',
+  'vendor/foundation/js/foundation.min.js',
+  'src/app.js'
+];
+var STYLES = [
+  'vendor/foundation/css/normalize.css',
+  'vendor/foundation/css/foundation.css',
+  'src/app.css'
+];
+var DATA = [
+  'data/data.json'
+];
+
+var server = express();
+server.use(express.logger());
+server.use(express.compress());
+server.use(express.json());
+server.use(express.urlencoded());
+server.use(express.static(path.join(__dirname, 'build')));
+
+gulp.task('clean', function() {
+  return gulp.src(BUILD, {read: false})
+    .pipe(clean());
+});
+
+gulp.task('serve', function() {
+  server.listen(SERVER_PORT);
+});
+
+gulp.task('data', function() {
+  gulp.src(DATA).pipe(gulp.dest(BUILD));
+});
+
+gulp.task('scripts', function() {
+  gulp.src(MODERNIZR).pipe(gulp.dest(BUILD));
+  gulp.src(SCRIPTS)
+    .pipe(concat(SCRIPTS_CONCAT))
+    .pipe(gulp.dest(BUILD));
+});
+
+gulp.task('styles', function() {
+  gulp.src(STYLES)
+    .pipe(minifyCSS())
+    .pipe(concat(STYLES_CONCAT))
+    .pipe(gulp.dest(BUILD));
+});
+
+gulp.task('html', function() {
+  gulp.src('index.html')
+    .pipe(gulp.dest(BUILD));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/**', ['scripts', 'styles']);
+  gulp.watch('data/**', ['data']);
+  gulp.watch('index.html', ['html']);
+});
+
+gulp.task('default', [
+  'data',
+  'scripts',
+  'styles',
+  'html',
+  'serve',
+  'watch'
+]);
