29b7e74f2676d113358463cac43688c1d6a799b7
Gilles Fabio <gilles@gillesfabio.com>
2014-03-25 20:48:42 +0100

    Refactor generator.

--- generator.js
+++ generator.js
@@ -5,13 +5,16 @@ var path  = require('path');
 var _     = require('lodash');
 var YAML  = require('yamljs');
 
+var DATA_DIR = path.join(__dirname, 'data-refactor');
+var SCRAPER_JSON = path.join(__dirname, 'scrap-data.json');
+
 var Generator = module.exports = function Generator() {
   this.data = {
-    themes        : [],
-    pollingPlaces : [],
-    candidates    : [],
-    runningMates  : [],
-    programs      : []
+    themes     : [],
+    offices    : [],
+    candidates : [],
+    programs   : [],
+    lists      : []
   };
   this.rawData = {};
 };
@@ -25,109 +28,98 @@ Generator.getExtension = function getExtension(file) {
   return ext[ext.length - 1];
 };
 
-Generator.prototype.formatCandidates = function() {
-  Object.keys(this.rawData).forEach(function(key) {
-    var candidate = this.rawData[key];
-    this.data.candidates.push(candidate.profil);
-  }.bind(this));
+Generator.prototype.buildThemes = function() {
+  var file = path.join(DATA_DIR, 'themes.yaml');
+  this.data.themes = YAML.load(file);
 };
 
-Generator.prototype.formatRunningMates = function() {
-  Object.keys(this.rawData).forEach(function(key) {
-    var candidate = this.rawData[key];
-    if (candidate.colistiers) {
-      candidate.colistiers.forEach(function(colistier) {
-        colistier.candidate = candidate.profil;
-        this.data.runningMates.push(colistier);
-      }.bind(this));
-    }
-  }.bind(this));
-};
-
-Generator.prototype.formatPrograms = function() {
-  Object.keys(this.rawData).forEach(function(key) {
-    var candidate = this.rawData[key];
-    if (candidate.programme) {
-      var program = {};
-      program.candidate = candidate.profil;
-      program.projects = [];
-      Object.keys(candidate.programme).forEach(function(key) {
-        var theme = _.find(this.data.themes, function(obj) { return obj.id === key; });
-        candidate.programme[key].forEach(function(rawProject) {
-          var project = {};
-          project.theme = theme;
-          project.description = rawProject;
-          program.projects.push(project);
-        });
-      }.bind(this));
-      this.data.programs.push(program);
-    }
-  }.bind(this));
-};
-
-Generator.prototype.candidates = function() {
-  var basedir = path.join(__dirname, 'data', 'candidates');
-  fs.readdirSync(basedir).forEach(function(dir) {
-    var stat = fs.statSync(path.join(basedir, dir));
-    if (stat.isDirectory()) {
-      this.rawData[dir] = {};
-      fs.readdirSync(path.join(basedir, dir)).forEach(function(file) {
-        var ext = Generator.getExtension(file);
-        var name = Generator.getFilename(file);
-        if (ext === 'yaml') {
-          this.rawData[dir][name] = YAML.load(path.join(basedir, dir, file));
-        }
-      }.bind(this));
-    }
-  }.bind(this));
-};
-
-Generator.prototype.themes = function() {
-  var file = path.join(__dirname, 'data', 'themes.yaml');
-  if (fs.existsSync(file)) this.data.themes = YAML.load(file);
-};
-
-Generator.prototype.results = function() {
-  var file = path.join(__dirname, 'data', 'results.yaml');
-  var items = YAML.load(file);
-  this.data.results = [];
-  items.forEach(function(raw) {
-    var obj = _.clone(raw);
-    obj.candidates = [];
-    raw.candidates.forEach(function(candidate) {
-      var o = _.clone(candidate);
-      o.candidate = _.find(this.data.candidates, {id: candidate.candidate});
-      obj.candidates.push(o);
-    }.bind(this));
-    this.data.results.push(obj);
-  }.bind(this));
-};
-
-Generator.prototype.pollingPlaces = function() {
-  var file = path.join(__dirname, 'data', 'polling-places.yaml');
+Generator.prototype.buildOffices = function() {
+  var file = path.join(DATA_DIR, 'offices.yaml');
   var yaml = YAML.load(file);
   Object.keys(yaml).forEach(function(key) {
-    var obj = {};
-    var raw = yaml[key];
-    obj.number = key;
-    obj.address = raw.address;
-    obj.opening = raw.opening;
+    var obj        = {};
+    var raw        = yaml[key];
+    obj.number     = key;
+    obj.address    = raw.address;
+    obj.opening    = raw.opening;
     obj.gmapSearch = raw.gmapSearch;
-    this.data.pollingPlaces.push(obj);
+    this.data.offices.push(obj);
+  }.bind(this));
+};
+
+Generator.prototype.buildCandidates = function() {
+  var file = path.join(DATA_DIR, 'candidates.yaml');
+  this.data.candidates = YAML.load(file);
+};
+
+Generator.prototype.buildPrograms = function() {
+  var dir = path.join(DATA_DIR, 'programs');
+  fs.readdirSync(dir).forEach(function(file) {
+    file              = path.join(DATA_DIR, 'programs', file);
+    var id            = path.basename(file).split('.')[0];
+    var yaml          = YAML.load(file);
+    var program       = {};
+    program.candidate = _.find(this.data.candidates, function(obj) { return obj.id === id; });
+    program.projects  = [];
+    Object.keys(yaml).forEach(function(key) {
+      var theme    = _.find(this.data.themes, function(obj) { return obj.id === key; });
+      var projects = yaml[key];
+      projects.forEach(function(rawProject) {
+        var project         = {};
+        project.theme       = theme;
+        project.description = rawProject;
+        program.projects.push(project);
+      });
+    }.bind(this));
+    this.data.programs.push(program);
+  }.bind(this));
+};
+
+Generator.prototype.buildLists = function() {
+  var file   = fs.readFileSync(SCRAPER_JSON);
+  var data   = JSON.parse(file)['data'];
+  var rounds = [
+    [1, data.r1.lists],
+    [2, data.r2.lists]
+  ];
+  rounds.forEach(function(round) {
+    var lists = round[1];
+    Object.keys(lists).forEach(function(id) {
+      lists[id].forEach(function(rawMate) {
+        var mate       = {};
+        mate.round     = round[0];
+        mate.candidate = _.find(this.data.candidates, function(obj) { return obj.id === id; });
+        mate.name      = rawMate.name;
+        mate.position  = rawMate.position;
+        mate.cc        = rawMate.cc;
+        this.data.lists.push(mate);
+      }.bind(this));
+    }.bind(this));
+  }.bind(this));
+};
+
+Generator.prototype.buildResults = function() {
+  var file   = fs.readFileSync(SCRAPER_JSON);
+  var data   = JSON.parse(file)['data'];
+  var rounds = [
+    [1, data.r1.results],
+    [2, data.r2.results]
+  ];
+  rounds.forEach(function(round) {
+    var results = round[1];
+    Object.keys(lists).forEach(function(id) {
+
   }.bind(this));
 };
 
 Generator.prototype.build = function() {
 
-  this.themes();
-  this.pollingPlaces();
-  this.candidates();
-
-  this.formatCandidates();
-  this.formatRunningMates();
-  this.formatPrograms();
-
-  this.results();
+  this.buildThemes();
+  this.buildOffices();
+  this.buildCandidates();
+  this.buildPrograms();
+  this.buildLists();
+  this.buildResults();
 
   return this.data;
 };
