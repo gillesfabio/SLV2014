f2048d7460b8298afbe96b09e324f788e3fb7e76
Gilles Fabio <gilles@gillesfabio.com>
2014-03-26 06:39:16 +0100

    Rename RunningMateList view test.

--- test/frontend/views/ListTest.js
+++ test/frontend/views/ListTest.js
@@ -1,15 +1,15 @@
 define([
 
   'chai',
-  'App.views.RunningMateList',
-  'App.collections.RunningMate',
+  'App.views.List',
+  'App.collections.List',
   'App.collections.Candidate',
   'App.models.Candidate'
 
 ], function(
   chai,
   View,
-  RunningMateCollection,
+  ListCollection,
   CandidateCollection,
   CandidateModel) {
 
@@ -20,29 +20,29 @@ define([
   var expect = chai.expect;
 
   describe('App.views', function() {
-    describe('App.views.RunningMateListTest', function() {
+    describe('App.views.ListTest', function() {
 
       describe('#initialize', function() {
         it('should properly set defaults', function() {
           var view = new View();
           expect(view.candidate).to.be.an.instanceof(CandidateModel);
-          expect(view.runningMates).to.be.an.instanceof(RunningMateCollection);
+          expect(view.lists).to.be.an.instanceof(ListCollection);
         });
       });
 
       describe('#getTemplateContext', function() {
         it('should return a proper context', function() {
-          var view = new View();
+          var view    = new View();
           var context = view.getTemplateContext();
-          expect(context).to.have.keys(['config', 'runningMates']);
+          expect(context).to.have.keys(['config', 'people']);
         });
         it('should set the mandatory config.baseUrl variable in context', function() {
-          var view = new View();
+          var view    = new View();
           var context = view.getTemplateContext();
           expect(context.config).to.contain.keys('baseUrl');
         });
-        it('should set runningMates in context', function(done) {
-          var col = new RunningMateCollection();
+        it('should set people in context', function(done) {
+          var col        = new ListCollection();
           var candidates = new CandidateCollection();
           candidates.fetch({
             success: function() {
@@ -50,9 +50,9 @@ define([
               col.fetch({
                 success: function() {
                   var models  = col.findByCandidate(candidate.get('id'));
-                  var view    = new View({candidate: candidate, runningMates: col});
+                  var view    = new View({candidate: candidate, lists: col});
                   var context = view.getTemplateContext();
-                  expect(context.runningMates).to.deep.equal(models.toJSON());
+                  expect(context.people).to.deep.equal(models.toJSON());
                   done();
                 }
               });
--- test/frontend/views/ResultListTest.js
+++ test/frontend/views/ResultListTest.js
@@ -25,12 +25,12 @@ define([
 
       describe('#getTemplateContext', function() {
         it('should properly set template context', function() {
-          var view = new View();
+          var view    = new View();
           var context = view.getTemplateContext();
           expect(context).to.contain.keys(['config']);
         });
         it('should properly set the mandatory config.baseUrl', function() {
-          var view = new View();
+          var view    = new View();
           var context = view.getTemplateContext();
           expect(context.config).to.contain.keys('baseUrl');
         });
@@ -39,7 +39,7 @@ define([
             {round: 1, candidates: []},
             {round: 2, candidates: []}
           ]);
-          var view = new View({round: 2, results: col});
+          var view    = new View({round: 2, results: col});
           var context = view.getTemplateContext();
           expect(context.results).to.deep.equal({round: 2, candidates: []});
         });
