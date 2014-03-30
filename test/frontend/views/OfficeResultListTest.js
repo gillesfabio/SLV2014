e911110cf0e0f288db1c07a64714eb161ccb9fce
Gilles Fabio <gilles@gillesfabio.com>
2014-03-30 19:07:00 +0200

    Add OfficeResultList view tests.

--- test/frontend/views/OfficeResultListTest.js
+++ test/frontend/views/OfficeResultListTest.js
@@ -1,9 +1,15 @@
 define([
 
   'chai',
-  'App.views.OfficeResultList'
+  'App.views.OfficeResultList',
+  'App.collections.OfficeResult',
+  'text!../fixtures/officesResults.json'
 
-], function(chai, View) {
+], function(
+  chai,
+  View,
+  OfficeResultCollection,
+  officesResultsFixtures) {
 
   /* jshint expr:true */
 
@@ -11,12 +17,23 @@ define([
 
   var expect = chai.expect;
 
+  officesResultsFixtures = JSON.parse(officesResultsFixtures);
+
   describe('App.views', function() {
     describe('App.views.OfficeResultListTest', function() {
 
+      describe('#initialize', function() {
+        it('should properly set defaults', function() {
+          var view = new View();
+          expect(view.round).to.equal(1);
+          expect(view.results).to.be.an.instanceof(OfficeResultCollection);
+        });
+      });
+
       describe('#getTemplateContext', function() {
         it('should return a proper context', function() {
-          var view    = new View();
+          var col     = new OfficeResultCollection(officesResultsFixtures);
+          var view    = new View({results: col, round: 2});
           var context = view.getTemplateContext();
           expect(context).to.have.keys([
             'config',
@@ -24,6 +41,9 @@ define([
             'results',
             'candidateStats'
           ]);
+          expect(context.round).to.equal(2);
+          expect(context.results).to.deep.equal(col.findByRound(2).toJSON());
+          expect(context.candidateStats).to.deep.equal(col.findByRound(2).getCandidateTopFlopStats());
         });
         it('should set the mandatory config.baseUrl variable in context', function() {
           var view    = new View();
